5a3362c36f66510d8287f62d612383b0c611a63d
heroku20 <130670798+heroku20@users.noreply.github.com>
2023-05-15 14:47:38 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,188 @@
+ <?php
+echo '
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+    <!-- Site made with Mobirise Website Builder v5.8.9, https://mobirise.com -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="generator" content="Mobirise v5.8.9, mobirise.com">
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:image:src" content="assets/images/index-meta.webp">
+    <meta property="og:image" content="assets/images/index-meta.webp">
+    <meta name="twitter:title" content="GlucoTrust - Official Site">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+    <link rel="shortcut icon" href="assets/images/screenshot-2023-05-13-at-3.53.16-pm-358x74.png" type="image/x-icon">
+    <meta name="description" content="Support vision health with Ocuprime\'s proprietary  blend of 24 powerful ingredients carefully selected from nature">
+
+
+    <title>GlucoTrust - Official Site</title>
+    <link rel="stylesheet" href="assets/web/assets/mobirise-icons2/mobirise2.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="assets/dropdown/css/style.css">
+    <link rel="stylesheet" href="assets/socicon/css/styles.css">
+    <link rel="stylesheet" href="assets/theme/css/style.css">
+    <link rel="preload" href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap" as="style" onload="this.onload=null;this.rel=\'stylesheet\'">
+    <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap"></noscript>
+    <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css">
+    <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+
+
+
+
+</head>
+
+<body>
+
+    <section data-bs-version="5.1" class="menu menu1 cid-tnIXGkiqCz" once="menu" id="menu">
+
+
+        <nav class="navbar navbar-dropdown navbar-fixed-top navbar-expand-lg">
+            <div class="container">
+                <div class="navbar-brand">
+                    <span class="navbar-logo">
+                    <a href="index.html">
+                        <img src="assets/images/screenshot-2023-05-13-at-3.53.16-pm-358x74.png" alt="ocuprime" style="height: 3rem;">
+                    </a>
+                </span>
+
+                </div>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-bs-toggle="collapse" data-target="#navbarSupportedContent" data-bs-target="#navbarSupportedContent" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <div class="hamburger">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav nav-dropdown" data-app-modern-menu="true">
+                        <li class="nav-item"><a class="nav-link link text-info text-primary display-4" href="https://bestofferhere.link/GlucoTrust">
+                            Yes, Give Me My Discount Link!</a></li>
+                        <li class="nav-item"><a class="nav-link link text-info text-primary display-4" href="https://bestofferhere.link/GlucoTrust">
+                            Benefits</a></li>
+                    </ul>
+
+                    <div class="navbar-buttons mbr-section-btn"><a class="btn btn-primary display-4" href="https://bestofferhere.link/GlucoTrust"><span class="mobi-mbri mobi-mbri-arrow-next mbr-iconfont mbr-iconfont-btn"></span><font face="Moririse2"><span style="font-size: 22px;">&nbsp;</span></font>ORDER NOW</a></div>
+                </div>
+            </div>
+        </nav>
+    </section>
+
+    <section data-bs-version="5.1" class="features15 cid-tnJaAwlyFR" id="features16-o">
+
+
+
+
+        <div class="container">
+            <div class="content-wrapper">
+                <div class="row align-items-center">
+                    <div class="col-12 col-lg">
+                        <div class="text-wrapper">
+                            <h1 class="card-title mbr-fonts-style display-2"><strong>GlucoTrust</strong></h1>
+                            <p class="mbr-text mbr-fonts-style mb-4 display-7">GlucoTrust is a stunning dietary supplement that supports regulating the blood sugar level with the help of the ultimate nutrition content present in the ingredients.<br><br>Get your Own <strong>Ocuprime&nbsp;</strong>with
+                                51% Off&nbsp;plus<br><strong>180 day Money Back Guarantee.</strong></p>
+                            <div class="mbr-section-btn mt-3"><a class="btn btn-success display-7" href="https://bestofferhere.link/GlucoTrust">&nbsp;Yes, Give Me My Discount Link!</a> <a class="btn btn-primary display-7" href="https://bestofferhere.link/GlucoTrust"><span class="mobi-mbri mobi-mbri-arrow-next mbr-iconfont mbr-iconfont-btn" style="font-size: 22px;"></span>Official Site</a></div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-4">
+                        <div class="image-wrapper">
+                            <a href="https://bestofferhere.link/GlucoTrust"><img src="assets/images/glucotrust-1-bottle-removebg-preview-466x352.png" alt="ocuprime"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section data-bs-version="5.1" class="features16 cid-tnKdrOkc85" id="features17-10">
+
+
+
+        <div class="container">
+            <div class="content-wrapper">
+                <div class="row align-items-center">
+                    <div class="col-12 col-lg-4">
+                        <div class="image-wrapper">
+                            <a href="https://bestofferhere.link/GlucoTrust"><img src="assets/images/money-back-guarantee.webp" alt="money back guarantee"></a>
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg">
+                        <div class="text-wrapper">
+                            <h3 class="card-title mbr-fonts-style display-5"><strong>180-DAY MONEY BACK GUARANTEE</strong></h3>
+                            <p class="mbr-text mbr-fonts-style mb-4 display-7">GlucoTrust comes with a 100% money back guarantee - 180 full days from your original purchase. If for any reason at all you want to get your money back.<br></p>
+
+                            <p class="mbr-text mbr-fonts-style mb-4 display-7">You will get it within 24 hours from the moment you sendme the request. So you have nothing to lose here.<br></p>
+                            <div class="mbr-section-btn mt-3"><a class="btn btn-success display-4" href="https://bestofferhere.link/GlucoTrust"><span class="mobi-mbri mobi-mbri-arrow-next mbr-iconfont mbr-iconfont-btn"></span>Click To Order From Official Website</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section data-bs-version="5.1" class="image3 cid-tnP8J57xQS" id="image3-1v">
+
+
+
+
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12 col-lg-6">
+                    <div class="image-wrapper">
+                        <img src="assets/images/ocuprime-763x148.jpg" alt="ocuprime qualities">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section data-bs-version="5.1" class="image3 cid-tD0vg6mk0m" id="image3-4n">
+
+
+
+
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12 col-lg-4">
+                    <div class="image-wrapper">
+                        <a href="https://bestofferhere.link/GlucoTrust"><img src="assets/images/betabeat-buy.gif" alt="order ocuprime"></a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section data-bs-version="5.1" class="footer7 cid-tovw8QKy7q" once="footers" id="footer7-4g">
+
+
+
+
+
+        <div class="container">
+            <div class="media-container-row align-center mbr-white">
+                <div class="col-12">
+                    <p class="mbr-text mb-0 mbr-fonts-style display-7">
+                        © Copyright 2023&nbsp;<a href="" class="text-danger" target="_blank">G</a>lucoTrust - All Rights Reserved<br><a href="" class="text-danger text-primary" target="_blank">Privacy Policy</a> - <a href="" class="text-danger text-primary"
+                            target="_blank">Terms</a> - <a href="" class="text-danger text-primary" target="_blank">Disclaimer</a><br>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <script src="assets/smoothscroll/smooth-scroll.js"></script>
+        <script src="assets/ytplayer/index.js"></script>
+        <script src="assets/dropdown/js/navbar-dropdown.js"></script>
+        <script src="assets/theme/js/script.js"></script>
+
+
+        <div id="scrollToTop" class="scrollToTop mbr-arrow-up"><a style="text-align: center;"><i class="mbr-arrow-up-icon mbr-arrow-up-icon-cm cm-icon cm-icon-smallarrow-up"></i></a></div>
+</body>
+
+</html>
+';
+?>
